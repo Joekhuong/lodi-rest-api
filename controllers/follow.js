4354ef7c510916fa48963bbe01129b07b971f69c
Khuong Chu <joe@x7.fi>
2019-02-18 08:23:55 +0200

    Modify follow API

--- controllers/follow.js
+++ controllers/follow.js
@@ -9,12 +9,23 @@ module.exports = {
     routes: [
       {
         action: "follow",
+        path: "follows_idol",
         method: "post"
       },
       {
         action: "unfollow",
+        path: "unfollows_idol",
         method: "post"
-      }
+      },
+      {
+        action: "getForUser",
+        path: "user_id/:userid",
+        method: "post"
+      },
+      {
+        action: "hasFollowed",
+        path: "check_follow/:userid/:idolid"
+      },
     ]
   },
 
@@ -22,8 +33,8 @@ module.exports = {
 
     model(req).destroy({
        where: {
-          user_id: req.body.user_id,
-          idol_id: req.body.idol_id
+          user_id: req.params.user_id,
+          idol_id: req.params.idol_id
        }
     }).then(rowDeleted => {
       res.json({status:true});
@@ -32,23 +43,45 @@ module.exports = {
   },
 
   follow: (req, res, next) => {
+    console.log(req.params);
     model(req)
       .create({
-        user_id: req.body.user_id,
-        idol_id: req.body.idol_id
+        user_id: req.params.user_id,
+        idol_id: req.params.idol_id
       })
       .then(row => {
-        res.json(row);
+        console.log(row);
+        res.json({status:true});
       })
-      .catch(() => res.status(404));;
+      .catch((err) => res.json({status:false,err}));
   },
 
   hasFollowed: (req, res, next) => {
 
+    const Op = req.app.locals.sequelize.Op;
+
     model(req).find({
        where: {
-          user_id: req.body.user_id,
-          idol_id: req.body.idol_id
+          [Op.and]: [
+            { user_id: req.params.userid },
+            { idol_id: req.params.idolid }
+          ]
+       }
+    })
+    .then(row => {
+
+      if(row == null) res.json({status:false});
+      console.log(row);
+      row.length > 0 ? res.json({status:true}) : res.json({status:false});
+    })
+    .catch(() => (res.status(404)));
+  },
+
+  getForUser: (req, res, next) => {
+
+    model(req).find({
+       where: {
+          user_id: req.params.userid
        }
     })
     .then(row => {
